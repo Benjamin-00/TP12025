7d128b5f619d6ef3680648e750ebb5f2c91be2fe
Benjamin <124399721+Benjamin-00@users.noreply.github.com>
2025-04-23 18:06:46 -0300

    Update 00-ingesta.R

--- 00-ingesta.R
+++ 00-ingesta.R
@@ -2,17 +2,20 @@
 # install.packages("googledrive")
 # install.packages("readxl")
 
+library(readxl)
+library(googledrive)
+
 # Descargo el archivo mediante su id de google drive
 # El link de los archivos de drive tiene esta forma:
-# https://docs.google.com/spreadsheets/d/16_zhdrZIW72I45SHIsVkGv-KYQw1oeup
-# El id de esta hoja de cálculo es "16_zhdrZIW72I45SHIsVkGv-KYQw1oeup"
-googledrive::drive_download(as_id("16_zhdrZIW72I45SHIsVkGv-KYQw1oeup"), 
-														overwrite = T)
+# https://docs.google.com/spreadsheets/d/1IRhvzOQkvuspQF3TAsBCI-68i8ya0_hy/edit?usp=sharing&ouid=108660883433416512120&rtpof=true&sd=true
+# El id de esta hoja de cálculo es "1IRhvzOQkvuspQF3TAsBCI-68i8ya0_hy"
+googledrive::drive_download(as_id("1IRhvzOQkvuspQF3TAsBCI-68i8ya0_hy"), 
+                            overwrite = T)
 
 # Cargo el archivo como .xlsx
-datos <- readxl::read_excel("arbol.xlsx", 
-														col_names = FALSE, 
-														skip = 3)
+datos <- readxl::read_excel("Datos_LP.xlsx", 
+                            col_names = FALSE, 
+                            skip = 3)
 
 # Veo la estructura del dataset
 str(datos)
