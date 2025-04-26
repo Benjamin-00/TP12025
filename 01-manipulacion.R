e81c75286a0ffecbc160b61bd40b9bbeb2d08fec
Benjamin <124399721+Benjamin-00@users.noreply.github.com>
2025-04-26 19:03:09 -0300

    Update 01-manipulacion.R

--- 01-manipulacion.R
+++ 01-manipulacion.R
@@ -10,7 +10,7 @@ attach(datos)
 ######################
 # Renombrar columnas #
 ######################
-colnames(datos)[c(2, 92, 117)] <- c("provincia", "plagas", "recoleccionDeBasura")
+colnames(datos)[c(2, 4, 7, 90, 92, 93, 94, 95, 117)] <- c("provincia","TiempoVivido", "CantFamilias", "alumbrado", "plagas", "cuarachas", "mosquitos", "ratas", "recoleccionDeBasura")
 
 ###################
 # Modificar datos #
@@ -20,7 +20,7 @@ datos_cuyo <- datos %>%
   filter(provincia %in% c("Mendoza", "La Rioja")) %>%
   
   mutate(
-    # transformaciones
+    alumbrado = recode(alumbrado, "Sí, hechas por el Estado (municipio, provincia o Estado nacional)" = "Sí, hechas por el Estado")
   )
 ##########################################
 # Seleccionar un subconjunto de columnas #
@@ -32,9 +32,22 @@ datos_analisis_1 <- datos_cuyo %>%
     recoleccionDeBasura, plagas
   )
 
+datos_analisis_2 <- datos_cuyo %>%
+  select(   # Seleccionar las columnas que quiero conservar
+    TiempoVivido, CantFamilias
+  )
+
+datos_analisis_3 <- datos_cuyo %>%
+  select(   # Seleccionar las columnas que quiero conservar
+    TiempoVivido, alumbrado 
+  )
+
+tipo_plagas <- datos_cuyo %>%
+  select(cuarachas, mosquitos, ratas)
+
+datos_tipo_plagas <- tipo_plagas %>%
+  pivot_longer(cols = everything(), names_to = "plaga", values_to = "presencia") %>%
+  filter(!is.na(presencia))
 
 
-###########################################
-# Seleccionar un subconjunto de registros #
-###########################################
 
